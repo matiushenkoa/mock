0818e76a328789edad9137f5527c1aeadf90182e
Artem Matiushenko <matiushenko.artemm@gmail.com>
2025-04-01 14:49:25 +0200

    Update guess.js

--- guess.js
+++ guess.js
@@ -1,17 +1,19 @@
-const PICKED_NUM = 11;
+(function main() {
+    const PICKED_NUM = 11;
 
-const guess = (num) => {
-    if (num === PICKED_NUM) {
-        return 0;
+    const guess = (num) => {
+        if (num === PICKED_NUM) {
+            return 0;
+        }
+
+        if (num > PICKED_NUM) {
+            return -1;
+        }
+
+        if (num < PICKED_NUM) {
+            return 1;
+        }
     }
 
-    if (num > PICKED_NUM) {
-        return -1;
-    }
-
-    if (num < PICKED_NUM) {
-        return 1;
-    }
-}
-
-window.guess = guess;
+    window.guess = guess;
+}());
