52bcd94ade00bc30e7b1eb7c24463061e8551eac
Artem Matiushenko <matiushenko.artemm@gmail.com>
2025-04-01 14:39:10 +0200

    Create guess.js

---
+++ guess.js
@@ -0,0 +1,17 @@
+const PICKED_NUM = 11;
+
+const guess = (num) => {
+    if (num === PICKED_NUM) {
+        return 0;
+    }
+
+    if (num > PICKED_NUM) {
+        return -1;
+    }
+
+    if (num < PICKED_NUM) {
+        return 1;
+    }
+}
+
+window.guess = guess;
